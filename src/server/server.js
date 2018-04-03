281bc2d0132239d0e5089e60876b9a2d0497ed3d
Priyatam Vinnakota <vinnakota4201@gmail.com>
2018-04-03 16:24:09 +0530

    added health check route

--- src/server/server.js
+++ src/server/server.js
@@ -1,11 +1,14 @@
 var express    = require('express');
-var path    = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 12234;
+const PORT = 8080;
 
 app.get('/', function(req, res){
   res.json({ message: 'Leadership Dashboard!' });
+});
+
+app.get('/health', function(req, res){
+  res.json({ message: 'Leadership Dashboard UI working!' });
 });
 
 app.listen(PORT, function(){

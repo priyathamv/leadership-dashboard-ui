1390e73401f2b9f0e74107c5c162d2b303c124e3
Priyatam Vinnakota <vinnakota4201@gmail.com>
2018-04-03 23:02:04 +0530

    reverted server.js file

--- src/server/server.js
+++ src/server/server.js
@@ -1,16 +1,21 @@
-var express    = require('express');
+import express from 'express';
 
 const app = express();
 const PORT = 8080;
 
-app.get('/', function(req, res){
-  res.json({ message: 'Leadership Dashboard!' });
-});
+const staticPath = express.static(path.join(__dirname, '../'));
+const indexPath = path.join(__dirname, '../index.html');
+
+app.use("/static", staticPath);
 
 app.get('/health', function(req, res){
   res.json({ message: 'Leadership Dashboard UI working!' });
 });
 
-app.listen(PORT, function(){
-  console.log("Listening on port: " + PORT);
+app.get('/', (req, res) => {
+  res.sendFile(indexPath);
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
 });

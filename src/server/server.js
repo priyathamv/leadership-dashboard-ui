e594cba964102a70f635771101521e46f868fb5c
Priyatam Vinnakota <vinnakota4201@gmail.com>
2018-04-03 16:07:21 +0530

    removed es6

--- src/server/server.js
+++ src/server/server.js
@@ -8,6 +8,6 @@ app.get('/', function(req, res){
   res.json({ message: 'Leadership Dashboard!' });
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, function(){
   console.log("Listening on port: " + PORT);
 });
